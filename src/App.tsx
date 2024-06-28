9f38553e80e8f22989b34b774b1f274e6a8c890a
Yure <yurecod@gmail.com>
2024-06-28 11:03:18 -0400

    create: páginas e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
-import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
